bd6ffd9d1f28acfb5a56c68c922f12c9e8fc9f16
SenpaiKishore <108696612+SenpaiKishore@users.noreply.github.com>
2024-05-04 16:43:28 +0530

    Refactored the getAustralianStudentName function to not use any methods and use only native for loops

--- getAustralianStudentNames.js
+++ getAustralianStudentNames.js
@@ -1,11 +1,15 @@
 //    Create a function that extracts and displays the names of individuals who are students (`isStudent: true`) and live in Australia.
 function getAustralianStudentNames(arrayOfObjects) {
     try{
-        const australianStudents = arrayOfObjects.filter(person => person.isStudent && person.country === 'Australia');
-        const names = australianStudents.map(student => student.name);
+        const names = [];
+        for(let person of arrayOfObjects){
+            if(person.isStudent && person.country == "Australia"){
+                names.push(person.name);
+            }
+        }
         console.log(`Names of Australian students: ${names.join(', ')}`);
-    } catch {
-        console.log("Invalid input.")
+    } catch (error) {
+        console.log(`Invalid input. ${error}`);
     }
     
 }
