b991e5a8ef1f32f10e1aa64f67f2cafd689fa4ad
SenpaiKishore <108696612+SenpaiKishore@users.noreply.github.com>
2024-05-04 16:50:07 +0530

    Refactored the getHobbiesForAge to use only native for loops and not higher order array methods.

--- getHobbiesForAge.js
+++ getHobbiesForAge.js
@@ -1,10 +1,11 @@
 //    Implement a function that retrieves and prints the hobbies of individuals with a specific age, say 30 years old.
 function getHobbiesForAge(arrayOfObjects, age) {
     try {
-        const personsWithAge = arrayOfObjects.filter(person => person.age === age);
-        personsWithAge.forEach(person => {
-            console.log(`${person.name}'s hobbies (${age} years old): ${person.hobbies.join(', ')}`);
-        });
+        for(let person of arrayOfObjects){
+            if(person.age == age){
+                console.log(`${person.name}'s hobbies (${age} years old): ${person.hobbies.join(', ')}`);
+            }
+        }
     } catch (error) {
         console.log(`Invalid input. ${error}`);
     }
