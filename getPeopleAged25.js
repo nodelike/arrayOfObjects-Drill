f6f7f3c1e27f4d1b71df93c7ec53690f0c77ad15
SenpaiKishore <108696612+SenpaiKishore@users.noreply.github.com>
2024-05-04 16:51:52 +0530

    Refactored the getPeopleAged25 to use only native for loops and not higher order array methods.

--- getPeopleAged25.js
+++ getPeopleAged25.js
@@ -1,10 +1,11 @@
 //    Write a function that accesses and prints the names and email addresses of individuals aged 25.
 function getPeopleAged25(arrayOfObjects) {
     try {
-        const peopleAged25 = arrayOfObjects.filter(person => person.age === 25);
-        peopleAged25.forEach(person => {
-            console.log(`Name: ${person.name}, Email: ${person.email}`);
-        });
+        for(let person of arrayOfObjects){
+            if(person.age == 25){
+                console.log(`Name: ${person.name}, Email: ${person.email}`);
+            }
+        }
     } catch (error) {
         console.log(`Invalid input. Error: ${error}`);
     }
