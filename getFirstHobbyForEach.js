3016b98910972c9464ee8152a41d39c5853b926e
SenpaiKishore <108696612+SenpaiKishore@users.noreply.github.com>
2024-05-04 16:47:57 +0530

    Refactored the getFirstHobbyForEach to use only native for loops.

--- getFirstHobbyForEach.js
+++ getFirstHobbyForEach.js
@@ -1,9 +1,9 @@
 //    Create a function to retrieve and display the first hobby of each individual in the dataset.
 function getFirstHobbyForEach(arrayOfObjects) {
     try {
-        arrayOfObjects.forEach(person => {
+        for(let person of arrayOfObjects){
             console.log(`${person.name}'s first hobby: ${person.hobbies[0]}`);
-        });
+        }
     } catch (error) {
         console.log(`Invalid input. ${error}`);
     }   
